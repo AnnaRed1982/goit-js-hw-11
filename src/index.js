2a72ac7da747e54f42e300bc5e19cf6604df5583
annared <annared1982@gmail.com>
2022-12-27 22:08:46 +0200

    css

--- src/index.js
+++ src/index.js
@@ -40,16 +40,16 @@ function renderImages({ hits }) {
                 <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
                 <div class="info">
                     <p class="info-item">
-                        <b>Likes${hit.likes}</b>
+                        <b>Likes ${hit.likes}</b>
                     </p>
                     <p class="info-item">
-                        <b>Views${hit.views}</b>
+                        <b>Views ${hit.views}</b>
                     </p>
                     <p class="info-item">
-                        <b>Comments${hit.comments}</b>
+                        <b>Comments ${hit.comments}</b>
                     </p>
                     <p class="info-item">
-                        <b>Downloads${hit.downloads}</b>
+                        <b>Downloads ${hit.downloads}</b>
                     </p>
                 </div>
              </div>`;
