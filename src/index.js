6d373077c31419f69adb8cc448a2df520ab88915
annared <annared1982@gmail.com>
2022-12-30 23:21:19 +0200

    1

--- src/index.js
+++ src/index.js
@@ -21,9 +21,7 @@ function onSearch(evt) {
 
   searchQuery = evt.currentTarget.elements.searchQuery.value;
   API.resetPage();
-
   API.fetchImages(searchQuery).then(render.renderImages).catch(onCatchError);
-
   buttonLoadMore.classList.remove('is-hidden');
 }
 
