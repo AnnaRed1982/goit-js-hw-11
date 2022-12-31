4f341a6f4b54eea46c3f4077280254712d51fb91
annared <annared1982@gmail.com>
2022-12-31 15:36:23 +0200

    api

--- src/API.js
+++ src/API.js
@@ -1,19 +1,23 @@
+const axios = require('axios').default;
+
 const API_KEY = '32381232-0d08b52c11723d23aba771294';
 const per_page = 40;
 let page = 1;
 
-function fetchImages(searchQuery) {
-  return fetch(
-    `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${page}`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-
+async function fetchImages(searchQuery) {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${page}`
+    );
+    console.log(response);
     page += 1;
-    return response.json();
-  });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    Notiflix.Notify.failure(`${error}`);
+  }
 }
+
 function resetPage() {
   page = 1;
 }
@@ -24,3 +28,16 @@ function paginatNumber() {
   return per_page;
 }
 export default { fetchImages, resetPage, returnPage, paginatNumber };
+
+// function fetchImages(searchQuery) {
+//   return fetch(
+//     `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${page}`
+//   ).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+
+//     page += 1;
+//     return response.json();
+//   });
+// }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './css/styles.css';
 import API from './API';
 import render from './renderIMG';
 import Notiflix from 'notiflix';
-const axios = require('axios').default;
+
 
 const formREF = document.querySelector('.search-form');
 const galleryREF = document.querySelector('.gallery');
