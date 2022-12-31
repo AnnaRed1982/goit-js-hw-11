547118fca701ce4aec505c48c3a2e74e57775194
annared <annared1982@gmail.com>
2022-12-31 10:57:20 +0200

    1

--- src/renderIMG.js
+++ src/renderIMG.js
@@ -36,26 +36,36 @@ function renderImages({ hits, totalHits }) {
 
   const markup = hits
     .map(hit => {
-      return `<div class="photo-card">
-                <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy"/>
-                <div class="info">
-                    <p class="info-item">
-                        <b>Likes</b>${hit.likes}
-                    </p>
-                    <p class="info-item">
-                        <b>Views</b>${hit.views}
-                    </p>
-                    <p class="info-item">
-                        <b>Comments</b>${hit.comments}
-                    </p>
-                    <p class="info-item">
-                        <b>Downloads</b>${hit.downloads}
-                    </p>
-                </div>
-             </div>`;
+      return `
+                  <a class="gallery__item" href="${hit.largeImageURL}">
+                    <div class="photo-card">
+                        <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy"/>
+                        <div class="info">
+                            <p class="info-item">
+                                <b>Likes</b>${hit.likes}
+                            </p>
+                            <p class="info-item">
+                                <b>Views</b>${hit.views}
+                            </p>
+                            <p class="info-item">
+                                <b>Comments</b>${hit.comments}
+                            </p>
+                            <p class="info-item">
+                                <b>Downloads</b>${hit.downloads}
+                            </p>
+                        </div>
+                    </div>
+                 </a>
+             `;
     })
     .join('');
   galleryREF.insertAdjacentHTML('beforeend', markup);
+
+  var lightbox = new SimpleLightbox('.gallery a', {
+    // sourceAttr: hits.largeImageURL,
+  });
+
+  lightbox.refresh();
 }
 
 export default { renderImages };
